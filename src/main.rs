886ff7f02d48cd5af417120c9826953b09bdc727
imtsuki <me@qjx.app>
2020-01-03 14:11:05 +0800

    loop -> hlt_loop
    Signed-off-by: imtsuki <me@qjx.app>

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,28 @@
 #![no_std]
 #![no_main]
+#![feature(asm)]
 #![feature(global_asm)]
 
 use core::panic::PanicInfo;
 
 global_asm!(include_str!("boot.s"));
 
+macro_rules! hlt_loop {
+    () => {
+        loop {
+            unsafe {
+                asm!("hlt");
+            }
+        }
+    };
+}
+
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-    loop {}
+    hlt_loop!();
 }
 
 #[no_mangle]
 pub extern "C" fn bootloader_main() -> ! {
-    loop {}
+    hlt_loop!();
 }
