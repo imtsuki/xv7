5b1ff275472e90e891492655a2290c6d340b94cd
imtsuki <me@qjx.app>
2020-01-09 18:23:47 +0800

    uefi, workspace, cargo-make
    Signed-off-by: imtsuki <me@qjx.app>

---
+++ xv7-bootloader-uefi/src/main.rs
@@ -0,0 +1,36 @@
+#![no_std]
+#![no_main]
+#![feature(abi_efiapi)]
+
+#[macro_use]
+extern crate log;
+
+use core::mem;
+use uefi::prelude::*;
+use uefi::table::boot::MemoryDescriptor;
+
+#[no_mangle]
+extern "C" fn __rust_probestack() {}
+
+#[entry]
+fn efi_main(_image: Handle, st: SystemTable<Boot>) -> Status {
+    uefi_services::init(&st).expect_success("Failed to initialize utilities");
+    st.stdout().clear();
+    let bs = st.boot_services();
+    let current_time = st.runtime_services().get_time().unwrap().unwrap();
+    info!("Hello, UEFI");
+    info!(
+        "Now is {}/{}/{} {}:{}:{} {:?}",
+        current_time.year(),
+        current_time.month(),
+        current_time.day(),
+        current_time.hour(),
+        current_time.minute(),
+        current_time.second(),
+        current_time.time_zone()
+    );
+    let map_size = bs.memory_map_size();
+    info!("map_size: {}", map_size);
+
+    Status::SUCCESS
+}
