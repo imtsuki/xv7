fdf14752f3badfc5ecdfee2b3125e8eec532dfb2
imtsuki <me@qjx.app>
2020-01-10 20:32:36 +0800

    UEFI: load kernel image
    Signed-off-by: imtsuki <me@qjx.app>

--- xv7-bootloader-uefi/src/main.rs
+++ xv7-bootloader-uefi/src/main.rs
@@ -1,19 +1,28 @@
 #![no_std]
 #![no_main]
 #![feature(abi_efiapi)]
+#![feature(asm)]
+#![feature(box_patterns)]
+#![feature(box_syntax)]
 
+#[macro_use]
+extern crate alloc;
 #[macro_use]
 extern crate log;
 
+use alloc::vec::Vec;
 use chrono::prelude::*;
 use uefi::prelude::*;
+use uefi::proto::media::file::*;
+use uefi::proto::media::fs::SimpleFileSystem;
+use uefi::Result;
 
 #[no_mangle]
 extern "C" fn __rust_probestack() {}
 
 #[entry]
 fn efi_main(_image: Handle, system_table: SystemTable<Boot>) -> Status {
-    uefi_services::init(&system_table).expect_success("Failed to initialize utilities");
+    uefi_services::init(&system_table).expect_success("Failed to initialize UEFI environment");
     let _ = system_table.stdout().clear().unwrap();
 
     info!("Hello, UEFI");
@@ -28,6 +37,11 @@ fn efi_main(_image: Handle, system_table: SystemTable<Boot>) -> Status {
     info!("TimeZone Asia/Shanghai: {}", now);
 
     let boot_services = system_table.boot_services();
+
+    boot_services
+        .set_watchdog_timer(0, 0x10000, None)
+        .expect_success("Could not set watchdog timer");
+
     let map_size = boot_services.memory_map_size();
     info!("map_size: {}", map_size);
 
@@ -38,5 +52,59 @@ fn efi_main(_image: Handle, system_table: SystemTable<Boot>) -> Status {
         page_table.start_address()
     );
 
-    loop {}
+    info!(r"Load kernel image from \EFI\xv7\kernel");
+    let (len, data) =
+        read_file(boot_services, r"\EFI\xv7\kernel").expect_success("Could not load kernel image");
+
+    info!("Kernel image size = {}", len);
+    info!(
+        "Check kernel image magic number: {}{}{}",
+        data[1] as char, data[2] as char, data[3] as char
+    );
+
+    loop {
+        unsafe {
+            asm!("hlt");
+        }
+    }
+}
+
+pub fn read_file(service: &BootServices, path: &str) -> Result<(usize, Vec<u8>)> {
+    let fatfs = service
+        .locate_protocol::<SimpleFileSystem>()
+        .log_warning()?;
+    let fatfs = unsafe { &mut *fatfs.get() };
+
+    let mut volume = fatfs.open_volume().log_warning()?;
+
+    let file_handle = volume
+        .open(path, FileMode::Read, FileAttribute::empty())
+        .log_warning()?;
+
+    let mut file = match file_handle.into_type().log_warning()? {
+        FileType::Regular(regular) => regular,
+        FileType::Dir(_) => unreachable!(),
+    };
+
+    // Use an empty buffer to retrieve the actual FileInfo size
+    let mut empty_buf = Vec::new();
+    let len = match *file
+        .get_info::<FileInfo>(&mut empty_buf)
+        .expect_error("passing an empty buffer will return the size of FileInfo")
+        .data()
+    {
+        Some(len) => len,
+        None => unreachable!(),
+    };
+
+    let mut file_info = vec![0u8; len];
+    let file_info = file
+        .get_info::<FileInfo>(&mut file_info)
+        .discard_errdata()
+        .log_warning()?;
+
+    let mut buf = vec![0u8; file_info.file_size() as usize];
+    let len = file.read(&mut buf).discard_errdata().log_warning()?;
+
+    Ok((len, buf).into())
 }
