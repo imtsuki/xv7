7c2f22509322ab5ec91d3162b3e5a243ef4333cb
imtsuki <me@qjx.app>
2020-01-31 01:53:39 +0800

    Kernel: Simple display
    Signed-off-by: imtsuki <me@qjx.app>

--- xv7-kernel/src/main.rs
+++ xv7-kernel/src/main.rs
@@ -4,6 +4,38 @@
 
 mod lang_item;
 
+use embedded_graphics::{
+    drawable::Pixel,
+    egtext,
+    fonts::Font8x16,
+    geometry::Size,
+    pixelcolor::{Rgb888, RgbColor},
+    prelude::*,
+    text_style, DrawTarget,
+};
+
+struct GopDisplay {}
+
+impl DrawTarget<Rgb888> for GopDisplay {
+    #[inline(always)]
+    fn draw_pixel(&mut self, pixel: Pixel<Rgb888>) {
+        let Pixel(coord, color) = pixel;
+        let index = 800 * coord.y + coord.x;
+
+        unsafe {
+            core::ptr::write_volatile(
+                (0x80000000 as *mut u32).offset(index as isize),
+                ((color.r() as u32) << 16) | ((color.g() as u32) << 8) | (color.b() as u32),
+            );
+        }
+    }
+
+    #[inline(always)]
+    fn size(&self) -> Size {
+        Size::new(800, 600)
+    }
+}
+
 #[inline(always)]
 fn hlt_loop() -> ! {
     loop {
@@ -15,13 +47,16 @@ fn hlt_loop() -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let buffer = 0x80000000 as *mut u32;
+    let mut display = GopDisplay {};
 
-    for i in 0..(800 * 600) {
-        unsafe {
-            *buffer.offset(i) = 0xFFFFFFFF;
-        }
-    }
+    display.clear(RgbColor::WHITE);
+
+    egtext!(
+        text = "XV7: Yet Another Operating System by imtsuki",
+        top_left = (100, 100),
+        style = text_style!(font = Font8x16, text_color = Rgb888::BLACK)
+    )
+    .draw(&mut display);
 
     hlt_loop();
 }
