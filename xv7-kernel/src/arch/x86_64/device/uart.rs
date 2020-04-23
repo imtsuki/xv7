6e37b9b2b0b76b9c3d2db99c9901a3d9dbd2e4aa
imtsuki <me@qjx.app>
2020-04-23 13:21:17 +0800

    handle QEMU sending '\r' instead of '\n' when pressing enter on uart
    Signed-off-by: imtsuki <me@qjx.app>

--- xv7-kernel/src/arch/x86_64/device/uart.rs
+++ xv7-kernel/src/arch/x86_64/device/uart.rs
@@ -7,9 +7,15 @@ const COM1: u16 = 0x3f8;
 pub extern "x86-interrupt" fn handler(_stack_frame: &mut InterruptStackFrame) {
     let mut data_port = Port::<u8>::new(COM1);
     let mut line_sts_port = Port::<u8>::new(COM1 + 5);
-    if unsafe { line_sts_port.read() } & 0x01 != 0 {
-        let byte = unsafe { data_port.read() };
-        print!("{}", byte as char);
-    }
+
+    while unsafe { line_sts_port.read() } & 0x01 == 0 {}
+    let mut byte = unsafe { data_port.read() } as char;
+
+    // http://web.mit.edu/broder/Public/fixing-jos-serial.txt
+    byte = if byte == '\r' { '\n' } else { byte };
+    byte = if byte == '\x7f' { '\x08' } else { byte };
+
+    print!("{}", byte);
+
     LOCAL_APIC.lock().end_of_interrupt();
 }
